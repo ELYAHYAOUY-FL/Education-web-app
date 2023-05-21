cf6db65e44c4c60e267b910215ea9f5b2521c188
ELYAHYAOUY-FL <110742439+ELYAHYAOUY-FL@users.noreply.github.com>
2023-05-21 19:28:50 +0200

    animations scroll

--- resources/js/src/pages/AcuillePage.vue
+++ resources/js/src/pages/AcuillePage.vue
@@ -1,35 +1,40 @@
-
 <template>
     <div>
-    <div>
-     <header class="hero">
-        <Navbar/>
-         <Hero/>
-     </header>
-     <UsersCard/>
-         </div>
-         <div>
-            <Section/>
-         </div>
-         <Footer/>
-        </div>
-</template>
-
-<script>
-
- import Hero from '../component/header.vue'
- import Navbar from '../component/nav.vue'
- import UsersCard from '../component/UsersCard.vue'
- import Footer from '../component/footer.vue'
- import Section from '../component/section.vue'
-export default {
-    name :"AcuillePage",
-components :{
-    Section,
-    Footer,
-    Hero,
-    Navbar,
-    UsersCard
-}
-};
-</script>
+      <div>
+        <header class="hero" data-aos="fade-up-right">
+          <Navbar/>
+          <Hero/>
+        </header>
+        <UsersCard data-aos="fade-up-right"/>
+      </div>
+      <div>
+        <Section data-aos="zoom-in"/>
+      </div>
+      <Footer data-aos="slide-left" />
+    </div>
+  </template>
+  
+  <script>
+  import Hero from '../component/header.vue'
+  import Navbar from '../component/nav.vue'
+  import UsersCard from '../component/UsersCard.vue'
+  import Footer from '../component/footer.vue'
+  import Section from '../component/section.vue'
+  import AOS from 'aos'
+  import 'aos/dist/aos.css'
+  
+  export default {
+    name: "AcuillePage",
+    components: {
+      Section,
+      Footer,
+      Hero,
+      Navbar,
+      UsersCard
+    },
+    mounted() {
+      AOS.init();
+    }
+  };
+  </script>
+  
